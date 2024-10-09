243e0dc55de7f5fdec1b12b5a8f25a29af103263
Daniil Ryzhenko <ryzhenko62@gmail.com>
2024-10-09 20:11:09 +0500

    icon delete

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -2,10 +2,9 @@ import { FunctionComponent } from "react";
 
 interface ButtonProps {
     children: string
-    icon?: Element
 }
  
-const Button: FunctionComponent<ButtonProps> = ({children, icon}) => {
+const Button: FunctionComponent<ButtonProps> = ({children}) => {
     return (
         <button className="bg-[#0078EE] h-[50px] rounded-md text-white hover:bg-blue-500 hover:transition-all duration-500">
             {children}
