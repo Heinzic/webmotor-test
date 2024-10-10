c16319cf7b17a92ef8f5cb24bd2cde17d2e16d28
Daniil Ryzhenko <ryzhenko62@gmail.com>
2024-10-11 00:42:24 +0500

    styles scroll

--- src/components/shared/TabTitle.tsx
+++ src/components/shared/TabTitle.tsx
@@ -15,7 +15,7 @@ const TabTitle: FunctionComponent<TabTitleProps> = ({children, titleIndex}) => {
         <div className="max-w-[718px]">
             <div className="flex justify-between">
                 <Title size="lg">Заголовок таба {titleIndex}</Title>
-                {width <1024 && <button>
+                {width < 1024 && <button>
                     <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect className="group-hover:fill-white" x="27" width="27" height="27" rx="5" transform="rotate(90 27 0)" fill="#0078EE"/>
                         <g clip-path="url(#clip0_346_4328)">
